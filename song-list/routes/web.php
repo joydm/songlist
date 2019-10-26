34824092ec86f33fe233eb2d40eb1c273ed48e42
Joy D <darendamasco@gmail.com>
2019-10-26 14:50:32 +0800

    Setting up Boostrap theme SB-Admin

--- song-list/routes/web.php
+++ song-list/routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo', function () {
+   return view('demo');
+});
+
+Route::get('dashboard', 'HomeController@dashboard');
